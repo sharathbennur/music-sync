c4a626e1deb3b3bb746e7c98e82a8c161e7d26c4
Sharath Bennur <sharath.bennur@gmail.com>
2025-04-13 22:18:03 -0400

    Updated code works, working on persisting update to local DB

--- scripts/sync.py
+++ scripts/sync.py
@@ -1,12 +1,13 @@
 import pickle
 from pathlib import Path
 from ytmusicapi import YTMusic
-from utils import YTM_Song
+from utils import YTM_Song, album_name, get_sync_status, in_library, is_explicit
+from pprint import pprint
 
 
 parent_dir_path = Path(__file__).resolve().parents[1]
-ytm = YTMusic("{}/data/browser.json".format(parent_dir_path.absolute()))
-# print(ytm.get_account_info())
+ytm = YTMusic("browser.json")
+print(ytm.get_account_info())
 
 
 def get_yt_search_results(item) -> list[YTM_Song]:
@@ -32,16 +33,19 @@ def get_yt_search_results(item) -> list[YTM_Song]:
         if search_results:
             for ind, result in enumerate(search_results):
                 if result['resultType']=="song":
-                    search_result_list.append(YTM_Song(
-                        artists = result['artists'],
-                        index = ind,
-                        album_name = result['album']['name'],
-                        type = result['resultType'],
-                        song_title = result['title'],
-                        in_library = result['inLibrary'],
-                        is_explicit = result['isExplicit'],
-                        duration = result['duration'],
-                        video_id = result['videoId']))
+                    try:
+                        search_result_list.append(YTM_Song(
+                            artists = result['artists'],
+                            index = ind,
+                            album_name = album_name(result),
+                            type = result['resultType'],
+                            song_title = result['title'],
+                            in_library = in_library(result),
+                            is_explicit = is_explicit(result),
+                            duration = result['duration'],
+                            video_id = result['videoId']))
+                    except KeyError:
+                        print(result)
 
     return search_result_list
 
@@ -60,8 +64,8 @@ def q_replace_song(search_result_list: list) -> dict:
             # and if there is an explicit version available
             if ytm_song.in_library:
                 in_library = True
-            if ytm_song.is_explicit:
-                is_explicit = True
+                if ytm_song.is_explicit:
+                    is_explicit = True
     return {
         "in_library": in_library,
         "is_explicit": is_explicit 
@@ -79,7 +83,12 @@ def q_which_song(search_result_list: list)-> int:
             for artist in song.artists:
                 song_artists = song_artists.join([song_artists, ('' if not song_artists else '+'),  artist['name']]).strip()
                 # song_artists = song_artists.join([song_artists, '+', artist['name']]) 
-            song_text = "{}.{}-{}-{}".format(idx, song.song_title, song_artists, song.album_name)
+            explicit_label = "(E)" if song.is_explicit else ""
+            song_text = "{}{}.{}-{}-{}".format(idx,
+                                                explicit_label,
+                                                song.song_title, 
+                                                song_artists, 
+                                                song.album_name)
             q_text = '\n'.join([q_text, song_text])
     print(q_text)
     selected_song = input("Select song # or 'e' to: ")
@@ -97,10 +106,20 @@ def validate_rate_song(index: int,
     """
     if index != None:
         replacement_song = search_result_list[index]
-        temp = ytm.rate_song(videoId=replacement_song.video_id, rating="LIKE")
-        print(temp)
+        try:
+            artist_names = [x["name"] for x in replacement_song.artists]
+            song_string = "{}: {}".format(" & ".join(artist_names),
+                                         replacement_song.song_title)
+            print(song_string)
+            ytm.rate_song(videoId=replacement_song.video_id, rating="LIKE")
+            print("Saved {} to liked music".format(song_string))
+            return True
+        except Exception as e:
+            print("Could not save song: {}".format(e))
+            return False
     else:
         print("No song selected, moving along...")
+        return True
         
 
 """
@@ -119,22 +138,34 @@ def sync_sp_ytm():
     with open("{}/data/spotify_saved_tracks.pickle".format(parent_dir_path.absolute()), 'rb') as pickle_file:
         spotify_library = pickle.load(pickle_file)
     for idx, item in enumerate(spotify_library):
-        # for now just index==1
-        if idx == 1:
+        synced = get_sync_status(item)
+        if not synced:
             search_result_list = get_yt_search_results(item)
             # if you have search results - you get a list back
             if search_result_list:
                 test_results = q_replace_song(search_result_list)
                 if test_results["in_library"] and not test_results["is_explicit"]:
+                    # if we have it in the lib but its not the explicit version
                     replacement_song_index = q_which_song(search_result_list)
+                    synced = validate_rate_song(replacement_song_index, search_result_list)
+                    print("Synced")
                 elif test_results["in_library"] and test_results["is_explicit"]:
+                    # if its already in the library and is explcit
                     pass
                 else:
+                    # if we don't have it in the libarary
                     replacement_song_index = q_which_song(search_result_list)
-                    print(replacement_song_index)
-                    validate_rate_song(replacement_song_index, search_result_list)
-        else:
-            pass
+                    synced = validate_rate_song(replacement_song_index, search_result_list)
+                    print("Synced")
+            else:
+                pass
+            # if we did successfuly sync this track to YTMusic
+            if synced:
+                item["done"] = True
+                spotify_library[idx] = item
+                with open('{}/data/spotify_saved_tracks.pickle'.format(parent_dir_path.absolute()), 'wb') as file:
+                    pickle.dump(spotify_library, file)
+                    print("saved updated to pickle file")
         
 
 sync_sp_ytm()
--- scripts/utils.py
+++ scripts/utils.py
@@ -12,3 +12,29 @@ class YTM_Song(NamedTuple):
     duration: str
     video_id: str
 
+def album_name(result: dict):
+    try: 
+        return result['album']['name']
+    except KeyError or TypeError:
+        return "no album name"
+    
+
+def in_library(result: dict):
+    try: 
+        return result['inLibrary']
+    except KeyError:
+        return False
+
+
+def is_explicit(result: dict):
+    try: 
+        return result['isExplicit']
+    except KeyError:
+        return False
+    
+
+def get_sync_status(item):
+    try:
+        return item["done"]
+    except KeyError:
+        return False
